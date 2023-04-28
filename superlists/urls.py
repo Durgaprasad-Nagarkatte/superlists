f122452f840d14fe4137ffae2ca674e3c5fde055
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2023-04-29 00:26:49 +0200

    Created new view returning HTML and wrote basic UTs

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', views.home_page, name='home')
 ]
